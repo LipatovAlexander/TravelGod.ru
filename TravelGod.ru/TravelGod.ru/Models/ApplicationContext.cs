932d20252f5d04ef5c1cda5c55660b298bed5d53
LipatovAlexander <lipatov.work@bk.ru>
2021-11-11 00:13:57 +0300

    add context

---
+++ TravelGod.ru/TravelGod.ru/Models/ApplicationContext.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+namespace TravelGod.ru.Models
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Trip> Trips { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Rating> Ratings { get; set; }
+        public DbSet<Chat> Chats { get; set; }
+        public DbSet<Message> Messages { get; set; }
+
+        public ApplicationContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            var splitStringConverter = new ValueConverter<IEnumerable<string>, string>(
+                v => string.Join(";", v),
+                v => v.Split(new[] { ';' }));
+            builder
+                .Entity<Trip>()
+                .Property(nameof(Trip.Route))
+                .HasConversion(splitStringConverter);
+        }
+    }
+}
