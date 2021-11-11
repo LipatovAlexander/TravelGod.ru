a558ee5e47004420733942ba82d7b4530fa0bcbc
LipatovAlexander <lipatov.work@bk.ru>
2021-11-11 20:13:30 +0300

    заглушка для профиля

---
+++ TravelGod.ru/TravelGod.ru/Pages/Profile.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using TravelGod.ru.Models;
+
+namespace TravelGod.ru.Pages
+{
+    public class Profile : PageModel
+    {
+        public new User User { get; set; }
+
+        public IActionResult OnGet()
+        {
+            if (User is null)
+            {
+                return RedirectToPage(nameof(SignIn));
+            }
+
+            return Page();
+        }
+    }
+}
