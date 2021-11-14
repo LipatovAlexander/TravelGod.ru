aae6107a383f5f09e966a02b4b4776c768f7091e
LipatovAlexander <lipatov.work@bk.ru>
2021-11-14 20:18:59 +0300

    добавил страницу профиля (редактирование)

--- TravelGod.ru/TravelGod.ru/Pages/Profile.cshtml.cs
+++ TravelGod.ru/TravelGod.ru/Pages/Profile.cshtml.cs
@@ -27,5 +27,27 @@ namespace TravelGod.ru.Pages
 
             return Page();
         }
+
+        public async Task<JsonResult> OnPostEdit(int id)
+        {
+            CurrentUser = await _context.Users.FirstOrDefaultAsync(u => u.Id == id);
+            if (CurrentUser.Id != User.Id)
+                return new JsonResult("Нет доступа!");
+
+            if (!await TryUpdateModelAsync(CurrentUser, "CurrentUser",
+                u => u.Description,
+                u => u.Email,
+                u => u.Patronymic,
+                u => u.BirthDate,
+                u => u.FirstName,
+                u => u.LastName) || !ModelState.IsValid)
+            {
+                return new JsonResult(new {Success = false});
+            }
+
+            _context.Users.Update(CurrentUser);
+            await _context.SaveChangesAsync();
+            return new JsonResult(new {Success = true});
+        }
     }
 }
---
+++ TravelGod.ru/TravelGod.ru/wwwroot/js/profile.js
@@ -0,0 +1,36 @@
+﻿$('#profile-form #toggle-edit').on('click', function (e) {
+    toggleEditable()
+})
+
+$('#profile-form input[type="submit"]').on('click', function () {
+    let form = $('#profile-form');
+    let validator = form.validate();
+    if (form.valid()) {
+        $.ajax({
+            type: "POST",
+            url: '/Profile?handler=Edit&id=' + $('#UserId').val(), // Replace YOUR_CUSTOM_HANDLER with your handler.
+            data: $('#profile-form').serialize(),
+            beforeSend: function (xhr) {
+                xhr.setRequestHeader("XSRF-TOKEN",
+                    $('input:hidden[name="__RequestVerificationToken"]').val());
+            },
+            dataType: "json"
+        }).done(function (data) {
+            if (data.success)
+                toggleEditable();
+        })
+    } else {
+        validator.showErrors();
+    }
+    return false;
+})
+
+function toggleEditable(editFlag) {
+    let inputs = $('#profile-form input:not([type="submit"])');
+    inputs.attr('readonly', (_, attr) => !attr);
+    inputs.attr('disabled', (_, attr) => !attr);
+    inputs.toggleClass('form-control-plaintext');
+    inputs.toggleClass('form-control');
+    $('#profile-form #toggle-edit').attr('hidden', (_, attr) => !attr);
+    $('#profile-form input[type="submit"]').attr('hidden', (_, attr) => !attr);
+}
