c30617ee8b91a9b8ba54a9d6518d529c970f7e9a
LipatovAlexander <lipatov.work@bk.ru>
2021-11-14 20:16:53 +0300

    изменил User (добавил описание, валидацию)

---
+++ TravelGod.ru/TravelGod.ru/Migrations/20211114110644_User_Desc_And_Validation.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TravelGod.ru.Migrations
+{
+    public partial class User_Desc_And_Validation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Patronymic",
+                table: "Users",
+                type: "varchar(20)",
+                maxLength: 20,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PasswordSalt",
+                table: "Users",
+                type: "longtext",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PasswordHash",
+                table: "Users",
+                type: "longtext",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Login",
+                table: "Users",
+                type: "varchar(10)",
+                maxLength: 10,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "Users",
+                type: "varchar(20)",
+                maxLength: 20,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "Users",
+                type: "varchar(20)",
+                maxLength: 20,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "BirthDate",
+                table: "Users",
+                type: "datetime(6)",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime(6)");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Users",
+                type: "varchar(100)",
+                maxLength: 100,
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "RememberMe",
+                table: "Sessions",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "RememberMe",
+                table: "Sessions");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Patronymic",
+                table: "Users",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(20)",
+                oldMaxLength: 20,
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PasswordSalt",
+                table: "Users",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PasswordHash",
+                table: "Users",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Login",
+                table: "Users",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(10)",
+                oldMaxLength: 10)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "Users",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(20)",
+                oldMaxLength: 20,
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "Users",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(20)",
+                oldMaxLength: 20,
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "BirthDate",
+                table: "Users",
+                type: "datetime(6)",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime(6)",
+                oldNullable: true);
+        }
+    }
+}
--- TravelGod.ru/TravelGod.ru/Models/User.cs
+++ TravelGod.ru/TravelGod.ru/Models/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace TravelGod.ru.Models
 {
@@ -12,17 +13,44 @@ namespace TravelGod.ru.Models
 
     public class User
     {
-        public DateTime BirthDate { get; set; }
+        [RegularExpression(@"^[А-Яа-яA-Za-z\.,!?""' ]*$", ErrorMessage = "Описание содержит недопустимые символы")]
+        [MaxLength(100, ErrorMessage = "Описание не должно быть длиннее 100 символов")]
+        public string Description { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime? BirthDate { get; set; }
+
         public List<Chat> JoinedChats { get; set; }
-        public string Email { get; set; }
+
+        [EmailAddress] public string Email { get; set; }
+
+        [RegularExpression(@"^[A-Za-zА-Яа-я]*$", ErrorMessage = "Имя содержит недопустимые символы")]
+        [MaxLength(20, ErrorMessage = "Имя не должно быть длиннее 20 символов")]
         public string FirstName { get; set; }
-        public int Id { get; set; }
+
+        [RegularExpression(@"^\d*$")] public int Id { get; set; }
+
+        [RegularExpression(@"^[A-Za-zА-Яа-я]*$", ErrorMessage = "Фамилия содержит недопустимые символы")]
+        [MaxLength(20, ErrorMessage = "Фамилия не должна быть длиннее 20 символов")]
         public string LastName { get; set; }
+
+        [Required]
+        [RegularExpression(@"^[A-Za-z0-9_]*$", ErrorMessage = "Логин содержит недопустимые символы")]
+        [MaxLength(10, ErrorMessage = "Логин не должен быть длиннее 10 символов")]
         public string Login { get; set; }
+
+        [Required]
         public string PasswordHash { get; set; }
+        [Required]
         public string PasswordSalt { get; set; }
+
+        [RegularExpression(@"^[A-Za-zА-Яа-я]*$", ErrorMessage = "Отчество содержит недопустимые символы")]
+        [MaxLength(20, ErrorMessage = "Отчество не должно быть длиннее 20 символов")]
         public string Patronymic { get; set; }
+
+        [Required]
         public Role Role { get; set; }
+        [Required]
         public Status Status { get; set; }
         public List<Trip> JoinedTrips { get; set; } = new();
         public List<Chat> OwnedChats { get; set; } = new();
