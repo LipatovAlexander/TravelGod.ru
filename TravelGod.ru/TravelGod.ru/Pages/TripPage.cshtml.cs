058f6fea69b8fdfa33e133459e2a8b2cd7ecc492
LipatovAlexander <lipatov.work@bk.ru>
2021-11-18 21:26:55 +0300

    заглушка для страницы конкретной поездки

---
+++ TravelGod.ru/TravelGod.ru/Pages/TripPage.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using TravelGod.ru.Models;
+using TravelGod.ru.Services;
+
+namespace TravelGod.ru.Pages
+{
+    public class TripPage : MyPageModel
+    {
+        public Trip Trip { get; set; }
+
+        public IActionResult OnGet(int id)
+        {
+            Trip = _tripService.GetTripAsync(id).Result;
+            if (Trip is null)
+            {
+                return NotFound();
+            }
+
+            return Page();
+        }
+
+        private readonly TripService _tripService;
+
+        public TripPage(TripService tripService)
+        {
+            _tripService = tripService;
+        }
+    }
+}
