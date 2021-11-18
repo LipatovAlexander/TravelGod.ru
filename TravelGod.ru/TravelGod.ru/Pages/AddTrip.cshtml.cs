1b753a2d1787aabfa16fc4b0ce88225689bb6eff
LipatovAlexander <lipatov.work@bk.ru>
2021-11-18 21:26:19 +0300

    добавил страницу добавления поездки

---
+++ TravelGod.ru/TravelGod.ru/Pages/AddTrip.cshtml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using TravelGod.ru.Models;
+using TravelGod.ru.Services;
+
+namespace TravelGod.ru.Pages
+{
+    public class AddTrip : MyPageModel
+    {
+        private readonly TripService _tripService;
+
+        public AddTrip(TripService tripService)
+        {
+            _tripService = tripService;
+        }
+
+        [BindProperty]
+        public Trip Trip { get; set; }
+
+        [BindProperty]
+        [Required(ErrorMessage = "Введите маршрут")]
+        [RegularExpression(@"^[A-Za-zА-Яа-я ,-]*$", ErrorMessage = "Маршрут содержит недопустимые символы")]
+        public string RouteRaw { get; set; }
+
+        public IActionResult OnGet()
+        {
+            if (User is null)
+            {
+                Console.WriteLine(true);
+                return RedirectToPage("/SignIn");
+            }
+
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            await _tripService.AddTripAsync(Trip, RouteRaw, User);
+            return RedirectToPage("TripPage", new {id = Trip.Id});
+        }
+    }
+}
---
+++ TravelGod.ru/TravelGod.ru/wwwroot/js/addtrip.js
@@ -0,0 +1,21 @@
+﻿$('.radio-group .radio').click(function(){
+    $('.radio-group .radio').removeClass('selected');
+    $(this).addClass('selected');
+    var val = $(this).attr('data-value');
+    //alert(val);
+    $('#radio-value').val(val);
+});
+
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+})
+
+$('.btnNext').click(function(e){
+    e.preventDefault();
+    $('.nav-tabs .active').parent().next('li').find('a').trigger('click');
+});
+
+$('.btnPrevious').click(function(e){
+    e.preventDefault();
+    $('.nav-tabs .active').parent().prev('li').find('a').trigger('click');
+});
