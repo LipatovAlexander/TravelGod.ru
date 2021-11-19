5093140059e033b38b9a381c770de0cb4b08a575
LipatovAlexander <lipatov.work@bk.ru>
2021-11-19 21:51:20 +0300

    константы с сообщениями об ошибках

---
+++ TravelGod.ru/TravelGod.ru/Infrastructure/ValidationMessages.cs
@@ -0,0 +1,10 @@
+﻿namespace TravelGod.ru.Infrastructure
+{
+    public static class ValidationMessages
+    {
+        public const string RequiredMessage =  "Заполните это поле.";
+        public const string MinLengthMessage = "Введено слишком мало символов.";
+        public const string MaxLengthMessage = "Превышено максимальное число символов.";
+        public const string RegularExpressionMessage = "Введено некорректное значение.";
+    }
+}
