5572824ecd21ce5180f898f68f04c77f9e011d7a
LipatovAlexander <lipatov.work@bk.ru>
2021-11-19 22:05:50 +0300

    добавил ViewModels

--- TravelGod.ru/TravelGod.ru/Pages/SignUp.cshtml.cs
+++ TravelGod.ru/TravelGod.ru/Pages/SignUp.cshtml.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using TravelGod.ru.Infrastructure.Cryptography;
 using TravelGod.ru.Models;
 using TravelGod.ru.Services;
+using TravelGod.ru.ViewModels;
 
 namespace TravelGod.ru.Pages
 {
@@ -17,22 +18,7 @@ namespace TravelGod.ru.Pages
         }
 
         [BindProperty]
-        [Required(ErrorMessage = "Введите логин")]
-        [MinLength(5, ErrorMessage = "Логин не должен быть короче 5 символов")]
-        [MaxLength(10, ErrorMessage = "Логин не должен быть длиннее 10 символов")]
-        [RegularExpression(@"^[A-Za-z0-9]*$", ErrorMessage = "Логин должен состоять из букв и цифр")]
-        public string Login { get; set; }
-
-        [BindProperty]
-        [Required(ErrorMessage = "Введите пароль")]
-        [MinLength(5, ErrorMessage = "Пароль не должен быть короче 5 символов")]
-        [MaxLength(10, ErrorMessage = "Пароль не должен быть длиннее 10 символов")]
-        [RegularExpression(@"^[A-Za-z0-9]{5,10}$", ErrorMessage = "Пароль должен состоять из букв и цифр")]
-        public string Password1 { get; set; }
-
-        [BindProperty]
-        [Compare(nameof(Password1), ErrorMessage = "Пароли должны совпадать")]
-        public string Password2 { get; set; }
+        public SignUpModel SignUpModel { get; set; }
 
         public IActionResult OnGet()
         {
@@ -51,9 +37,9 @@ namespace TravelGod.ru.Pages
                 return RedirectToPage(nameof(Profile));
             }
 
-            if (await _userService.GetUserAsync(Login) is not null)
+            if (await _userService.GetUserAsync(SignUpModel.Login) is not null)
             {
-                ModelState.AddModelError("Login", "Логин уже занят");
+                ModelState.AddModelError("SignUpModel.Login", "Логин уже занят");
             }
 
             if (!ModelState.IsValid)
@@ -62,11 +48,11 @@ namespace TravelGod.ru.Pages
             }
 
             var passwordSalt = Cryptography.GenerateRandomCryptographicKey(32);
-            var passwordHash = Cryptography.ComputeMd5HashString(Password1 + passwordSalt);
+            var passwordHash = Cryptography.ComputeMd5HashString(SignUpModel.Password1 + passwordSalt);
 
             var newUser = new User
             {
-                Login = Login,
+                Login = SignUpModel.Login,
                 PasswordHash = passwordHash,
                 PasswordSalt = passwordSalt
             };
---
+++ TravelGod.ru/TravelGod.ru/ViewModels/SignUpModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using TravelGod.ru.Infrastructure;
+
+namespace TravelGod.ru.ViewModels
+{
+    public class SignUpModel
+    {
+        [Required(ErrorMessage = ValidationMessages.RequiredMessage)]
+        [MinLength(5, ErrorMessage = ValidationMessages.MinLengthMessage)]
+        [MaxLength(10, ErrorMessage = ValidationMessages.MaxLengthMessage)]
+        [RegularExpression(@"^[A-Za-z0-9]*$", ErrorMessage = ValidationMessages.RegularExpressionMessage)]
+        public string Login { get; set; }
+
+        [Required(ErrorMessage = ValidationMessages.RequiredMessage)]
+        [MinLength(5, ErrorMessage = ValidationMessages.MinLengthMessage)]
+        [MaxLength(10, ErrorMessage = ValidationMessages.MaxLengthMessage)]
+        [RegularExpression(@"^[A-Za-z0-9]*$", ErrorMessage = ValidationMessages.RegularExpressionMessage)]
+        public string Password1 { get; set; }
+
+        [Compare(nameof(Password1), ErrorMessage = "Пароли должны совпадать")]
+        public string Password2 { get; set; }
+    }
+}
