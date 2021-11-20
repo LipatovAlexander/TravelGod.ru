dc67df63a9b7fb28e7c5d054fe2042780c13ca1c
LipatovAlexander <lipatov.work@bk.ru>
2021-11-20 23:35:49 +0300

    добавил поля для количества поездок у пользователя

---
+++ TravelGod.ru/TravelGod.ru/Migrations/20211120190727_User_TripsCount.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TravelGod.ru.Migrations
+{
+    public partial class User_TripsCount : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "JoinedTripsCount",
+                table: "Users",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "OwnedTripsCount",
+                table: "Users",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "RouteRaw",
+                table: "Trips",
+                type: "varchar(200)",
+                maxLength: 200,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "JoinedTripsCount",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "OwnedTripsCount",
+                table: "Users");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "RouteRaw",
+                table: "Trips",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(200)",
+                oldMaxLength: 200)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
