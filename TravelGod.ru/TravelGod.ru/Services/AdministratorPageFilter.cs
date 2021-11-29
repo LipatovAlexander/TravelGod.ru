f070c4c34eedc905537fae2d120a4d23f14ad0de
LipatovAlexander <lipatov.work@bk.ru>
2021-11-29 23:38:02 +0300

    Добавил фильтры доступа к страницам

---
+++ TravelGod.ru/TravelGod.ru/Services/AdministratorPageFilter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using TravelGod.ru.Models;
+
+namespace TravelGod.ru.Services
+{
+    public class AdministratorPageFilter : Attribute, IAsyncPageFilter
+    {
+        public async Task OnPageHandlerSelectionAsync(PageHandlerSelectedContext context)
+        {
+            await Task.CompletedTask;
+        }
+
+        public async Task OnPageHandlerExecutionAsync(PageHandlerExecutingContext context, PageHandlerExecutionDelegate next)
+        {
+            if (context.HttpContext.Items["User"] is User {Role: Role.Administrator or Role.Moderator})
+            {
+                await next();
+            }
+            else
+            {
+                context.Result = new BadRequestResult();
+            }
+        }
+    }
+}
---
+++ TravelGod.ru/TravelGod.ru/Services/AuthenticationPageFilter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using TravelGod.ru.Models;
+
+namespace TravelGod.ru.Services
+{
+    public class AuthenticationPageFilter : Attribute, IAsyncPageFilter
+    {
+        public async Task OnPageHandlerSelectionAsync(PageHandlerSelectedContext context)
+        {
+            await Task.CompletedTask;
+        }
+
+        public async Task OnPageHandlerExecutionAsync(PageHandlerExecutingContext context, PageHandlerExecutionDelegate next)
+        {
+            if (context.HttpContext.Items["User"] is User)
+            {
+                await next();
+            }
+            else
+            {
+                context.Result = new RedirectToPageResult("/SignIn");
+            }
+        }
+    }
+}
