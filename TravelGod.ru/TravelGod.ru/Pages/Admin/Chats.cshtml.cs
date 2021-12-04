5d833d726fbe0ffdbf43f62931bdb258ee7c542b
LipatovAlexander <lipatov.work@bk.ru>
2021-12-05 00:24:31 +0300

    админка для чатов

---
+++ TravelGod.ru/TravelGod.ru/Pages/Admin/Chats.cshtml.cs
@@ -0,0 +1,67 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using TravelGod.ru.Infrastructure;
+using TravelGod.ru.Models;
+using TravelGod.ru.Services;
+
+namespace TravelGod.ru.Pages.Admin
+{
+    public class Chats : MyPageModel
+    {
+        private readonly ChatService _chatService;
+
+        public Chats(ChatService chatService)
+        {
+            _chatService = chatService;
+        }
+
+        public PaginatedList<Chat> ListOfChats { get; set; }
+        public Chat EditedChat { get; set; }
+
+        public async Task<IActionResult> OnGet(int pageIndex = 1)
+        {
+            ListOfChats = await _chatService.GetChatsAsync(pageIndex);
+
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostEdit(int id)
+        {
+            EditedChat = await _chatService.GetChatAsync(id, null);
+
+            if (EditedChat is null)
+            {
+                return BadRequest();
+            }
+
+            if (!await TryUpdateModelAsync(EditedChat, nameof(EditedChat)))
+            {
+                return BadRequest();
+            }
+
+            ModelState.ClearValidationState(nameof(EditedChat));
+            if (!TryValidateModel(EditedChat, nameof(EditedChat)))
+            {
+                return BadRequest();
+            }
+
+            await _chatService.UpdateChatAsync(EditedChat);
+            return new JsonResult("success");
+        }
+
+        public async Task<IActionResult> OnGetRemove(int id, int pageIndex)
+        {
+            var chat = await _chatService.GetChatAsync(id, Status.Normal);
+            if (chat is null)
+            {
+                return BadRequest();
+            }
+
+            await _chatService.RemoveChatAsync(chat);
+
+            return RedirectToPage("/Admin/Chats",
+                new {pageIndex});
+        }
+    }
+}
--- TravelGod.ru/TravelGod.ru/Services/ChatService.cs
+++ TravelGod.ru/TravelGod.ru/Services/ChatService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using TravelGod.ru.Infrastructure;
 using TravelGod.ru.Models;
 
 namespace TravelGod.ru.Services
@@ -38,13 +39,25 @@ namespace TravelGod.ru.Services
             return groupChats.Concat(personalChats).ToList();
         }
 
-        public async Task<Chat> GetChatAsync(int chatId)
+        public async Task<PaginatedList<Chat>> GetChatsAsync(int pageIndex)
+        {
+            const int pageSize = 10;
+            var chats = _context.Chats
+                                .Include(c => c.Users)
+                                .ThenInclude(u => u.Avatar)
+                                .Include(c => c.Messages)
+                                .Include(c => c.Initiator)
+                                .ThenInclude(u => u.Avatar);
+            return await PaginatedList<Chat>.CreateAsync(chats, pageIndex, pageSize);
+        }
+
+        public async Task<Chat> GetChatAsync(int chatId, Status? status)
         {
             var chat = await _context.Chats
                                      .Include(c => c.Users.Where(u => u.Status == Status.Normal))
                                      .ThenInclude(u => u.Avatar)
                                      .Include(c => c.Messages.Where(m => m.Status == Status.Normal))
-                                     .FirstOrDefaultAsync(c => c.Id == chatId);
+                                     .FirstOrDefaultAsync(c => c.Id == chatId && (status == null || c.Status == status));
 
             return chat;
         }
@@ -60,6 +73,25 @@ namespace TravelGod.ru.Services
             };
             trip.Chat = chat;
             _context.Trips.Update(trip);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateChatAsync(Chat editedChat)
+        {
+            _context.Chats.Update(editedChat);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task RemoveChatAsync(Chat chat)
+        {
+            chat.Status = Status.RemovedByModerator;
+            foreach (var message in chat.Messages)
+            {
+                message.Status = Status.RemovedByModerator;
+            }
+
+            _context.Chats.Update(chat);
+            _context.Messages.UpdateRange(chat.Messages);
             await _context.SaveChangesAsync();
         }
     }
