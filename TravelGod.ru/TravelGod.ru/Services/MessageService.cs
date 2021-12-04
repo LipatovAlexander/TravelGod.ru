4e3b65a80b62d2f9fb0c5973521b4fa80653f58c
LipatovAlexander <lipatov.work@bk.ru>
2021-12-05 00:47:19 +0300

    админка для сообщений

--- TravelGod.ru/TravelGod.ru/Services/MessageService.cs
+++ TravelGod.ru/TravelGod.ru/Services/MessageService.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using TravelGod.ru.Infrastructure;
 using TravelGod.ru.Models;
 
 namespace TravelGod.ru.Services
@@ -18,6 +20,31 @@ namespace TravelGod.ru.Services
         {
             message.DateTime = DateTime.Now;
             _context.Messages.Add(message);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<PaginatedList<Message>> GetMessagesAsync(int pageIndex)
+        {
+            const int pageSize = 10;
+            var messages = _context.Messages
+                                   .Include(m => m.User)
+                                   .ThenInclude(u => u.Avatar)
+                                   .Include(m => m.Chat);
+            return await PaginatedList<Message>.CreateAsync(messages, pageIndex, pageSize);
+        }
+
+        public async Task<Message> GetMessageAsync(int id, Status? status)
+        {
+            return await _context.Messages
+                                 .Include(m => m.Chat)
+                                 .Include(m => m.User)
+                                 .ThenInclude(u => u.Avatar)
+                                 .FirstOrDefaultAsync(m => m.Id == id && (status == null || m.Status == status));
+        }
+
+        public async Task UpdateMessageAsync(Message editedMessage)
+        {
+            _context.Messages.Update(editedMessage);
             await _context.SaveChangesAsync();
         }
     }
