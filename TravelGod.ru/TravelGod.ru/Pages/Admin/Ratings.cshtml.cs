f71a3c5b22bb58b355a1b28b3b4fbbe95a0141eb
LipatovAlexander <lipatov.work@bk.ru>
2021-12-05 14:43:11 +0300

    добавил админку для отзывов

---
+++ TravelGod.ru/TravelGod.ru/Pages/Admin/Ratings.cshtml.cs
@@ -0,0 +1,68 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using TravelGod.ru.Infrastructure;
+using TravelGod.ru.Models;
+using TravelGod.ru.Services;
+
+namespace TravelGod.ru.Pages.Admin
+{
+    public class Ratings : MyPageModel
+    {
+        private readonly RatingService _ratingService;
+
+        public Ratings(RatingService ratingService)
+        {
+            _ratingService = ratingService;
+        }
+
+        public PaginatedList<Rating> ListOfRatings { get; set; }
+        public Rating EditedRating { get; set; }
+
+        public async Task<IActionResult> OnGet(int pageIndex = 1)
+        {
+            ListOfRatings = await _ratingService.GetRatingsAsync(pageIndex);
+
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostEdit(int id)
+        {
+            EditedRating = await _ratingService.GetRatingAsync(id, null);
+
+            if (EditedRating is null)
+            {
+                return BadRequest();
+            }
+
+            if (!await TryUpdateModelAsync(EditedRating, nameof(EditedRating)))
+            {
+                return BadRequest();
+            }
+
+            ModelState.ClearValidationState(nameof(EditedRating));
+            if (!TryValidateModel(EditedRating, nameof(EditedRating)))
+            {
+                return BadRequest();
+            }
+
+            await _ratingService.UpdateRatingAsync(EditedRating);
+            return new JsonResult("success");
+        }
+
+        public async Task<IActionResult> OnGetRemove(int id, int pageIndex)
+        {
+            var message = await _ratingService.GetRatingAsync(id, Status.Normal);
+            if (message is null)
+            {
+                return BadRequest();
+            }
+
+            message.Status = Status.RemovedByModerator;
+            await _ratingService.UpdateRatingAsync(message);
+
+            return RedirectToPage("/Admin/Ratings",
+                new {pageIndex});
+        }
+    }
+}
--- TravelGod.ru/TravelGod.ru/Services/RatingService.cs
+++ TravelGod.ru/TravelGod.ru/Services/RatingService.cs
@@ -1,4 +1,6 @@
 ﻿using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TravelGod.ru.Infrastructure;
 using TravelGod.ru.Models;
 
 namespace TravelGod.ru.Services
@@ -15,6 +17,30 @@ namespace TravelGod.ru.Services
         public async Task AddRatingAsync(Rating rating)
         {
             _context.Ratings.Add(rating);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<PaginatedList<Rating>> GetRatingsAsync(int pageIndex)
+        {
+            const int pageSize = 10;
+            var ratings = _context.Ratings
+                                  .Include(r => r.User)
+                                    .ThenInclude(u => u.Avatar)
+                                  .Include(r => r.Trip);
+            return await PaginatedList<Rating>.CreateAsync(ratings, pageIndex, pageSize);
+        }
+
+        public async Task<Rating> GetRatingAsync(int id, Status? status)
+        {
+            return await _context.Ratings
+                                 .Include(r => r.Trip)
+                                 .Include(r => r.User.Avatar)
+                                 .FirstOrDefaultAsync(r => r.Id == id && (status == null || r.Status == status));
+        }
+
+        public async Task UpdateRatingAsync(Rating editedRating)
+        {
+            _context.Ratings.Update(editedRating);
             await _context.SaveChangesAsync();
         }
     }
