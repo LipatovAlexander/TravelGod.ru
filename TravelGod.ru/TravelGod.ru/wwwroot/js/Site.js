9a784d5e43c252248340c09d78ba73ed62dbc4e4
LipatovAlexander <lipatov.work@bk.ru>
2021-12-25 17:56:44 +0300

    добавил индикацию аякс запросов (в процессе, успешно, ошибка)

--- TravelGod.ru/TravelGod.ru/wwwroot/js/Site.js
+++ TravelGod.ru/TravelGod.ru/wwwroot/js/Site.js
@@ -2,3 +2,21 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+$(function () {
+    $(document).bind("ajaxSend", function(){
+        $('#loaders').fadeIn(0);
+        $("#loading").fadeIn(0);
+    }).bind("ajaxSuccess", function(){
+        $("#loading").fadeOut(0);
+        $('#success').fadeIn(0);
+    }).bind("ajaxError", function(){
+        $("#loading").fadeOut(0);
+        $('#failure').fadeIn(0);
+    }).bind("ajaxComplete", function(){
+        $('#loaders').fadeOut(2000);
+        $('#success').fadeOut(2000);
+        $('#failure').fadeOut(2000);
+    });
+})
